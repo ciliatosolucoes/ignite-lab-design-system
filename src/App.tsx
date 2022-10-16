50675a742f86e96febae62fd0b78a85ca6eccfa4
Cleberson J. Ciliato <cleberson.ciliato@gmail.com>
2022-10-16 09:13:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <><h1>Hello World</h1><button className="bg-cyan-500 hover:bg-cyan-300">
+      Enviar
+    </button></>
+  )
+}
